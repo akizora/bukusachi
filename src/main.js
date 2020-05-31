087ef848597e0db9664bed3fea517310c34c7216
akiyama <cometogetherrightnow1@gmail.com>
2020-05-31 11:34:38 +0900

    temp commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // 20200307追加
+
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
